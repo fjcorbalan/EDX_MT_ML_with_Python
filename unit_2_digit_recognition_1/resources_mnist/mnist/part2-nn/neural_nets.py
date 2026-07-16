14fdd634c9c3f25f2b4bcea427437a034707f30c
Francisco Jose Corbalan De Maya <fjcorbalan85@gmail.com>
2026-07-16 12:33:40 +0200

    neural networks

--- unit_2_digit_recognition_1/resources_mnist/mnist/part2-nn/neural_nets.py
+++ unit_2_digit_recognition_1/resources_mnist/mnist/part2-nn/neural_nets.py
@@ -13,102 +13,119 @@ import math
 """
 
 
-def rectified_linear_unit(x):
-    """ Returns the ReLU of x, or the maximum between 0 and x."""
-    # TODO
+# def rectified_linear_unit(x):
+#     """ Returns the ReLU of x, or the maximum between 0 and x."""
+#     # TODO
+#     return max(0, x)
 
-def rectified_linear_unit_derivative(x):
-    """ Returns the derivative of ReLU."""
-    # TODO
+# def rectified_linear_unit_derivative(x):
+#     """ Returns the derivative of ReLU."""
+#     if x > 0:
+#         return 1
+#     return 0
 
-def output_layer_activation(x):
-    """ Linear function, returns input as is. """
-    return x
+# def output_layer_activation(x):
+#     """ Linear function, returns input as is. """
+#     return x
 
-def output_layer_activation_derivative(x):
-    """ Returns the derivative of a linear function: 1. """
-    return 1
+# def output_layer_activation_derivative(x):
+#     """ Returns the derivative of a linear function: 1. """
+#     return 1
 
-class NeuralNetwork():
-    """
-        Contains the following functions:
-            -train: tunes parameters of the neural network based on error obtained from forward propagation.
-            -predict: predicts the label of a feature vector based on the class's parameters.
-            -train_neural_network: trains a neural network over all the data points for the specified number of epochs during initialization of the class.
-            -test_neural_network: uses the parameters specified at the time in order to test that the neural network classifies the points given in testing_points within a margin of error.
-    """
+# class NeuralNetwork():
+#     """
+#         Contains the following functions:
+#             -train: tunes parameters of the neural network based on error obtained from forward propagation.
+#             -predict: predicts the label of a feature vector based on the class's parameters.
+#             -train_neural_network: trains a neural network over all the data points for the specified number of epochs during initialization of the class.
+#             -test_neural_network: uses the parameters specified at the time in order to test that the neural network classifies the points given in testing_points within a margin of error.
+#     """
 
-    def __init__(self):
+def __init__(self):
 
-        # DO NOT CHANGE PARAMETERS (Initialized to floats instead of ints)
-        self.input_to_hidden_weights = np.matrix('1. 1.; 1. 1.; 1. 1.')
-        self.hidden_to_output_weights = np.matrix('1. 1. 1.')
-        self.biases = np.matrix('0.; 0.; 0.')
-        self.learning_rate = .001
-        self.epochs_to_train = 10
-        self.training_points = [((2,1), 10), ((3,3), 21), ((4,5), 32), ((6, 6), 42)]
-        self.testing_points = [(1,1), (2,2), (3,3), (5,5), (10,10)]
+    #DO NOT CHANGE PARAMETERS (Initialized to floats instead of ints)
+    self.input_to_hidden_weights = np.matrix('1. 1.; 1. 1.; 1. 1.')
+    self.hidden_to_output_weights = np.matrix('1. 1. 1.')
+    self.biases = np.matrix('0.; 0.; 0.')
+    self.learning_rate = .001
+    self.epochs_to_train = 10
+    self.training_points = [((2,1), 10), ((3,3), 21), ((4,5), 32), ((6, 6), 42)]
+    self.testing_points = [(1,1), (2,2), (3,3), (5,5), (10,10)]
 
-    def train(self, x1, x2, y):
+def train(self, x1, x2, y):
 
-        ### Forward propagation ###
+    # Vectorising the provided scalar functions so they work on NumPy matrices
+        vec_relu = np.vectorize(rectified_linear_unit)
+        vec_relu_derivative = np.vectorize(rectified_linear_unit_derivative)
+        vec_output_act = np.vectorize(output_layer_activation)
+        vec_output_act_derivative = np.vectorize(output_layer_activation_derivative)
+
+        # Forward propagation ###
         input_values = np.matrix([[x1],[x2]]) # 2 by 1
 
         # Calculate the input and activation of the hidden layer
-        hidden_layer_weighted_input = # TODO (3 by 1 matrix)
-        hidden_layer_activation = # TODO (3 by 1 matrix)
+        hidden_layer_weighted_input = (self.input_to_hidden_weights * input_values) + self.biases # 3 by 1 matrix
+        hidden_layer_activation = vec_relu(hidden_layer_weighted_input) # 3 by 1 matrix
 
-        output =  # TODO
-        activated_output = # TODO
+        output = self.hidden_to_output_weights * hidden_layer_activation
+        activated_output = vec_output_act(output)
 
-        ### Backpropagation ###
+        # Backpropagation ###
 
         # Compute gradients
-        output_layer_error = # TODO
-        hidden_layer_error = # TODO (3 by 1 matrix)
+        output_layer_error = np.multiply((activated_output - y), vec_output_act_derivative(output))
+        
+        # Backpropagate error to hidden layer, factoring in the vectorized ReLU derivative
+        hidden_layer_error = np.multiply(self.hidden_to_output_weights.T * output_layer_error, vec_relu_derivative(hidden_layer_weighted_input))
 
-        bias_gradients = # TODO
-        hidden_to_output_weight_gradients = # TODO
-        input_to_hidden_weight_gradients = # TODO
+        bias_gradients = hidden_layer_error
+        hidden_to_output_weight_gradients = output_layer_error * hidden_layer_activation.T
+        input_to_hidden_weight_gradients = hidden_layer_error * input_values.T
 
         # Use gradients to adjust weights and biases using gradient descent
-        self.biases = # TODO
-        self.input_to_hidden_weights = # TODO
-        self.hidden_to_output_weights = # TODO
+        self.biases = self.biases - (self.learning_rate * bias_gradients)
+        self.input_to_hidden_weights = self.input_to_hidden_weights - (self.learning_rate * input_to_hidden_weight_gradients)
+        self.hidden_to_output_weights = self.hidden_to_output_weights - (self.learning_rate * hidden_to_output_weight_gradients)
 
-    def predict(self, x1, x2):
+def predict(self, x1, x2):
 
-        input_values = np.matrix([[x1],[x2]])
+    # Vectoring scalar functions so that they can handle NumPy matrices
+    vec_relu = np.vectorize(rectified_linear_unit)
+    vec_output_act = np.vectorize(output_layer_activation)
 
-        # Compute output for a single input(should be same as the forward propagation in training)
-        hidden_layer_weighted_input = # TODO
-        hidden_layer_activation = # TODO
-        output = # TODO
-        activated_output = # TODO
+    input_values = np.matrix([[x1],[x2]])
 
-        return activated_output.item()
+    # Compute output for a single input(should be same as the forward propagation in training)
+    hidden_layer_weighted_input = (self.input_to_hidden_weights * input_values) + self.biases # TODO
+    hidden_layer_activation = vec_relu(hidden_layer_weighted_input) # TODO
+    output = self.hidden_to_output_weights * hidden_layer_activation # TODO
+    activated_output = vec_output_act(output) # TODO
 
-    # Run this to train your neural network once you complete the train method
-    def train_neural_network(self):
+    return activated_output.item()
 
-        for epoch in range(self.epochs_to_train):
-            for x,y in self.training_points:
-                self.train(x[0], x[1], y)
+    return activated_output.item()
 
-    # Run this to test your neural network implementation for correctness after it is trained
-    def test_neural_network(self):
+#     Run this to train your neural network once you complete the train method
+#     def train_neural_network(self):
 
-        for point in self.testing_points:
-            print("Point,", point, "Prediction,", self.predict(point[0], point[1]))
-            if abs(self.predict(point[0], point[1]) - 7*point[0]) < 0.1:
-                print("Test Passed")
-            else:
-                print("Point ", point[0], point[1], " failed to be predicted correctly.")
-                return
+#         for epoch in range(self.epochs_to_train):
+#             for x,y in self.training_points:
+#                 self.train(x[0], x[1], y)
 
-x = NeuralNetwork()
+#     Run this to test your neural network implementation for correctness after it is trained
+#     def test_neural_network(self):
 
-x.train_neural_network()
+#         for point in self.testing_points:
+#             print("Point,", point, "Prediction,", self.predict(point[0], point[1]))
+#             if abs(self.predict(point[0], point[1]) - 7*point[0]) < 0.1:
+#                 print("Test Passed")
+#             else:
+#                 print("Point ", point[0], point[1], " failed to be predicted correctly.")
+#                 return
+
+# x = NeuralNetwork()
+
+# x.train_neural_network()
 
 # UNCOMMENT THE LINE BELOW TO TEST YOUR NEURAL NETWORK
 # x.test_neural_network()
